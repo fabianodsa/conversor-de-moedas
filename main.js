86bba7dc37958f3d75207d2f649bf31d14dfec44
fabianodsa <fabyanoduarte@hotmail.com>
2023-01-06 22:13:44 -0300

    Criando interface do projeto

---
+++ main.js
@@ -0,0 +1,4 @@
+const currencyOneEl = document.querySelector('[data-js="currency-one"]')
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]')
+
+console.log(currencyOneEl, currencyTwoEl)
